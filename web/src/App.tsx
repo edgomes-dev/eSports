ddd4bc18175f75c3c4ff66c5badb8aa0bed8c78d
edgomes-dev <edgomesgomes@outlook.com>
2023-03-18 11:54:22 -0300

    aula 05

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,55 @@
+import './styles/main.css'
+import * as Dialog from "@radix-ui/react-dialog"
+
+import logoImg from "./assets/logo-nlw-esports.svg"
+import { GameBanner } from './components/GameBanner'
+import { CreatedAdBanner } from './components/ CreateAdBanner'
+import { useEffect, useState } from 'react'
+import { GameController } from 'phosphor-react'
+import { Input } from './components/Form/input'
+import { CreateAdModal } from './components/CreateAdModal'
+import axios from 'axios'
+
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
+
 function App() {
-  return <h1>Hello World !</h1>
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    axios('http://localhost:3333/games')
+    .then(response => setGames(response.data))
+  }, [])
+  return (
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={logoImg} />
+
+      <h1 className='text-6xl text-white font-black mt-20'>
+        Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.
+      </h1>
+
+      <div className='grid grid-cols-6 gap-6 mt-16'>
+        {games.map((game: Game) => 
+          <GameBanner 
+            key={game.id} 
+            title={game.title} 
+            bannerUrl={game.bannerUrl} 
+            adsCount={game._count.ads} 
+          />)}
+      </div>
+
+      <Dialog.Root>
+        <CreatedAdBanner />
+        <CreateAdModal />  
+      </Dialog.Root>
+    </div>
+  )
 }
 
 export default App
